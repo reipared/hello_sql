8e073917ab91d9b68c8493f2bea9f88a88c79890
unicornhimbo <reinaldopared@gmail.com>
2024-11-10 12:55:49 -0400

    update

--- Statements/16_group_by.sql
+++ Statements/16_group_by.sql
@@ -12,3 +12,25 @@ FROM
   users
 GROUP BY
   age;
+
+SELECT
+  COUNT(age),
+  age
+FROM
+  users
+GROUP BY
+  age
+ORDER BY
+  age ASC;
+
+SELECT
+  COUNT(age),
+  age
+FROM
+  users
+WHERE
+  age > 15
+GROUP BY
+  age
+ORDER BY
+  age ASC;
