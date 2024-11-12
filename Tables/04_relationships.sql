ba8aad7ee0243680ab4331c6c35de2517026c5a1
unicornhimbo <reinaldopared@gmail.com>
2024-11-12 09:13:02 -0400

    create relationship

---
+++ Tables/04_relationships.sql
@@ -0,0 +1,26 @@
+CREATE TABLE
+  dni (
+    dni_id int AUTO_INCREMENT PRIMARY KEY,
+    dni_number int NOT NULL,
+    user_id int,
+    UNIQUE (dni_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+  );
+
+CREATE TABLE
+  companies (
+    company_id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL
+  );
+
+ALTER TABLE users ADD CONSTRAINT fk_companies FOREIGN KEY (company_id) REFERENCES companies (company_id);
+
+CREATE TABLE
+  users_languages (
+    users_language_id int AUTO_INCREMENT PRIMARY KEY,
+    user_id int,
+    language_id int,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (language_id) REFERENCES languages (language_id),
+    UNIQUE (user_id, language_id)
+  );
