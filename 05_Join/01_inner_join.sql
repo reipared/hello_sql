5336c0918cd63ce62dfd2128a45efa5d15b77c9a
unicornhimbo <reinaldopared@gmail.com>
2024-11-13 17:33:21 -0400

    Create Inner Join notes

---
+++ 05_Join/01_inner_join.sql
@@ -0,0 +1,47 @@
+SELECT
+  *
+FROM
+  users
+  INNER JOIN dni;
+
+SELECT
+  *
+FROM
+  users
+  INNER JOIN dni ON users.user_id = dni.user_id;
+
+SELECT
+  *
+FROM
+  users
+  JOIN dni ON users.user_id = dni.user_id;
+
+SELECT
+  name,
+  dni_number
+FROM
+  users
+  JOIN dni ON users.user_id = dni.user_id
+ORDER BY
+  age ASC;
+
+SELECT
+  *
+FROM
+  companies
+  JOIN users ON users.company_id = companies.company_id;
+
+SELECT
+  companies.name,
+  users.name
+FROM
+  companies
+  JOIN users ON companies.company_id = users.company_id;
+
+SELECT
+  users.name,
+  languages.name
+FROM
+  users_languages
+  JOIN users ON users_languages.user_id = users.user_id
+  JOIN languages ON users_languages.language_id = languages.language_id;
