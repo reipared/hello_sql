4aeb2fd87090ec6daae1431934299358dab46990
unicornhimbo <reinaldopared@gmail.com>
2024-11-13 08:47:48 -0400

    Update

--- Tables/04_relationships.sql
+++ Tables/04_relationships.sql
@@ -24,3 +24,43 @@ CREATE TABLE
     FOREIGN KEY (language_id) REFERENCES languages (language_id),
     UNIQUE (user_id, language_id)
   );
+
+INSERT INTO
+  dni (dni_number, user_id)
+VALUES
+  (11111111, 1);
+
+INSERT INTO
+  dni (dni_number, user_id)
+VALUES
+  (22222222, 2);
+
+INSERT INTO
+  dni (dni_number, user_id)
+VALUES
+  (33333333, 3);
+
+INSERT INTO
+  dni (dni_number)
+VALUES
+  (44444444);
+
+INSERT INTO
+  companies (name)
+VALUES
+  ('MoureDev');
+
+INSERT INTO
+  companies (name)
+VALUES
+  ('Apple');
+
+INSERT INTO
+  companies (name)
+VALUES
+  ('Google');
+
+INSERT INTO
+  companies (company_id)
+VALUES
+  (5);
--- Writing/02_update.sql
+++ Writing/02_update.sql
@@ -3,3 +3,86 @@ SET
   age = '21'
 WHERE
   user_id = 11
+UPDATE users
+SET
+  company_id = 2
+WHERE
+  user_id = 1;
+
+UPDATE users
+SET
+  company_id = 1
+WHERE
+  user_id = 3;
+
+UPDATE users
+SET
+  company_id = 3
+WHERE
+  user_id = 4;
+
+UPDATE users
+SET
+  company_id = 2
+WHERE
+  user_id = 7;
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('Swift');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('Kotlin');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('JavaScript');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('Java');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('Python');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('C#');
+
+INSERT INTO
+  languages (name)
+VALUES
+  ('COBOL');
+
+INSERT INTO
+  users_languages (user_id, language_id)
+VALUES
+  (1, 1);
+
+INSERT INTO
+  users_languages (user_id, language_id)
+VALUES
+  (1, 2);
+
+INSERT INTO
+  users_languages (user_id, language_id)
+VALUES
+  (1, 5);
+
+INSERT INTO
+  users_languages (user_id, language_id)
+VALUES
+  (2, 3);
+
+INSERT INTO
+  users_languages (user_id, language_id)
+VALUES
+  (2, 5);
