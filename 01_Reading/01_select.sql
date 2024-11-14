39dff89ab66d3e16e8d45dbe3c0bcac3d845c5f2
unicornhimbo <reinaldopared@gmail.com>
2024-11-14 08:58:25 -0400

    Comments update

--- 01_Reading/01_select.sql
+++ 01_Reading/01_select.sql
@@ -1,13 +1,16 @@
+-- Obtiene todos los datos de la tabla "users"
 SELECT
   *
 FROM
   users;
 
+-- Obtiene todos los nombres de la tabla "users"
 SELECT
   name
 FROM
   users;
 
+-- Obtiene el ID y el nombre de la tabla "users"
 SELECT
   user_id,
   name
