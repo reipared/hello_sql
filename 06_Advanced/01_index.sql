27005a01ddffba19de9de58fc0b73eb511a6996d
unicornhimbo <reinaldopared@gmail.com>
2024-11-14 08:26:07 -0400

    Update

---
+++ 06_Advanced/01_index.sql
@@ -0,0 +1,9 @@
+CREATE INDEX idx_name ON users (name);
+
+CREATE UNIQUE INDEX idx_name ON users (name);
+
+CREATE UNIQUE INDEX idx_name_surname ON users (name, surname);
+
+DROP INDEX idx_name ON users;
+
+DROP INDEX idx_name_surname ON users;
