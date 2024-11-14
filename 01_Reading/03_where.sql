ac6f915be618a16f74cc9c8b084d8cebdfc83f99
unicornhimbo <reinaldopared@gmail.com>
2024-11-14 09:02:42 -0400

    Comments update

--- 01_Reading/03_where.sql
+++ 01_Reading/03_where.sql
@@ -1,3 +1,4 @@
+-- Filtra todos los datos de la tabla users donde la edad sea 15
 SELECT
   *
 FROM
@@ -5,6 +6,7 @@ FROM
 WHERE
   age = 15;
 
+-- Filtra todos los nombres de la tabla users donde la edad sea 15
 SELECT
   name
 FROM
@@ -12,6 +14,7 @@ FROM
 WHERE
   age = 15;
 
+-- Filtra todos los nombres distintos de la tabla users donde la edad sea 15
 SELECT DISTINCT
   age
 FROM
